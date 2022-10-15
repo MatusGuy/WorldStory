e0a1d5ff1fd53d8ef7b7589a012df0b284bc2484
MatusGuy <mateu.axe@gmail.com>
2022-10-15 21:05:23 +0100

    command line option to start creator

--- WorldStory.pro
+++ WorldStory.pro
@@ -8,17 +8,24 @@ CONFIG += c++17
 # In order to do so, uncomment the following line.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
-SOURCES += $$files(*.cpp, true)
+SOURCES   += $$files(*.cpp, true)
+HEADERS   += $$files(*.h,   true)
+FORMS     += $$files(*.ui,  true)
+RESOURCES += $$files(*.qrc, true)
 
-HEADERS += $$files(*.h,   true)
+VERSION = 0.0.0.0
+QMAKE_TARGET = WorldStory
+QMAKE_TARGET_COMPANY = MatusGuy
+QMAKE_TARGET_DESCRIPTION = $$quote(World Story)
+QMAKE_TARGET_COPYRIGHT = $$quote(© 2022 MatusGuy)
+QMAKE_TARGET_PRODUCT = $$quote(World Story)
+QMAKE_TARGET_ORIGINAL_FILENAME = WorldStory.exe
+QMAKE_TARGET_INTERNALNAME = WS
 
-FORMS += $$files(classes/editor/*.ui, true)
+DEFINES += __VERSION__=$$quote(\\\"$${VERSION}\\\")
+DEFINES += __NAME__=$$quote(\\\"$${QMAKE_TARGET}\\\")
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
-
-RESOURCES += \
-    assets/assets.qrc \
-    creator/assets/creatorassets.qrc
--- main.cpp
+++ main.cpp
@@ -1,36 +1,79 @@
 #include "classes/gamedisplay.h"
 #include "classes/gridscene.h"
+#include "creator/classes/mainwindow.h"
 
 #include <QApplication>
-#include <QPixmap>
 #include <QFile>
 #include <QPoint>
+#include <QCommandLineParser>
+
+bool applyStyle(QString sheetname) {
+    QFile stylesheet(sheetname);
+    if (stylesheet.open(QFile::ReadOnly)) {
+        ((QApplication*) QApplication::instance())->setStyleSheet(stylesheet.readAll());
+        stylesheet.close();
+        return true;
+    }
+    return false;
+}
 
 namespace WS::Core {
-    inline void launchGame() {
+    inline int launchGame() {
         WS::Core::GameDisplay w;
 
         WS::Graphics::GridScene s(&w);
         w.setScene(&s);
 
-        WS::Levels::Level* l = WS::Levels::loadLevel(new QFile(":/test/levels/testLevel.xml"));
+        QFile lf(":/test/levels/testLevel.xml");
+        WS::Levels::Level* l = WS::Levels::loadLevel(&lf);
         s.setLevel(l);
 
         //l->content[1]->setPos(QPoint(0, s.pointSpacing));
 
         s.drawLoop();
         w.show();
+
+        return QApplication::exec();
     }
 
-    inline void launchCreator(QApplication* app) {
-        app->
+    inline int launchCreator() {
+        applyStyle(":/creator/style/appstyle.css");
+
+        WS::Creator::MainWindow w;
+        w.show();
+
+        return QApplication::exec();
+    }
+
+    inline int launchCreator(QFile levelFile) {
+        // later adding code for loading level file on main window class
+        return launchCreator();
     }
 }
 
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
+    a.setApplicationName(__NAME__);
+    a.setApplicationDisplayName(__NAME__);
+    a.setApplicationVersion(__VERSION__);
 
+    QCommandLineParser cli;
+    cli.setApplicationDescription("World Story game");
+    cli.addHelpOption();
+    cli.addVersionOption();
 
+    QCommandLineOption creatorOption(
+        QStringList() << "c" << "creator",
+        R"(Open creator on <levelFile> instead of game. If <levelFile> is "none" then open creator on an empty level)",
+        "levelFile"
+    );
+    cli.addOption(creatorOption);
 
-    return a.exec();
+    cli.process(a);
+
+    if (cli.isSet(creatorOption)) {
+        QString levelFile = cli.value(creatorOption);
+        if (levelFile == "none") return WS::Core::launchCreator();
+        else return WS::Core::launchCreator(QFile(levelFile));
+    } else return WS::Core::launchGame();
 }
