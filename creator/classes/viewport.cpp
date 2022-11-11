32695c624e7e7d58eacf4987cdd9b8824d61d050
MatusGuy <mateu.axe@gmail.com>
2022-11-11 17:30:28 +0000

    no viewport background cache

--- creator/classes/viewport.cpp
+++ creator/classes/viewport.cpp
@@ -5,5 +5,5 @@ using WS::Creator::Viewport;
 Viewport::Viewport(QWidget* parent): WS::Core::GameDisplay{parent} {
     setScene(&editorScene);
     setBackgroundBrush(QBrush(Qt::black));
-    setCacheMode(QGraphicsView::CacheBackground);
+    //setCacheMode(QGraphicsView::CacheBackground);
 }
